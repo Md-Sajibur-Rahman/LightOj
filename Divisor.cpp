395becd31f533e04e770af3923eed9926000afe3
Md-Sajibur-Rahman <155714121+Md-Sajibur-Rahman@users.noreply.github.com>
2024-01-05 16:28:54 +0600

    Add files via upload

---
+++ Divisor.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define do {
+#define done }
+#define start_ int main()
+#define finish return 0;
+using namespace std;
+
+
+
+
+start_
+do
+       ll t;cin>>t;
+       for(ll i=1;i<=t;i++)
+        do
+             ll n;cin>>n;
+             cout<<"Case "<<i<<": ";
+             for(ll i=1;i<=n;i++)
+                do
+                      if(n%i==0)
+                      do
+                           cout<<i<<" ";
+                      done
+                done
+                cout<<"\n";
+
+        done
+
+finish
+done
+
+
