f641d0ad0cb4bf3ca9626a43a15f22d84a4b46d2
Md-Sajibur-Rahman <155714121+Md-Sajibur-Rahman@users.noreply.github.com>
2024-01-05 15:20:21 +0600

    Add files via upload

---
+++ Decresing_order.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define do {
+#define done }
+#define start_ int main()
+#define finish return 0;
+using namespace std;
+
+
+void solve()
+do
+     ll cnt=0;
+     for(int i=1000;i>=1;i--)
+         do
+              cout<<i<<" ";
+              cnt++;
+              if(cnt%5==0)
+                  do
+                       cout<<endl;
+                  done
+
+         done
+done
+
+
+start_
+do
+
+              solve();
+
+
+
+finish
+done
+
